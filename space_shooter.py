daced02d305d464723cdd48a78b57cbe97e19eda
Sgryt87 <sergiigrytsaienko@gmail.com>
2018-09-10 19:58:01 -0500

    shooting is added :)

--- space_shooter.py
+++ space_shooter.py
@@ -16,6 +16,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
 # game init - ready
 pygame.init()
@@ -42,6 +43,11 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
     def update(self):
         self.speedx = 0
@@ -77,10 +83,28 @@ class Mob(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # remove if out of the screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 # all sprites
 all_sprites = pygame.sprite.Group()
 player = Player()
 mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 all_sprites.add(player)
 for i in range(8):
     m = Mob()
@@ -98,9 +122,27 @@ while running:
         # check window close
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
     # update
     all_sprites.update()
-    # draw/render
+
+    # check if the bullet hits a mob
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    # check to see if a mob hit a player
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+        print('Game Over')
+
+    ## draw/render
     # after  drawing everything, flip the display (when back end rendering is done, return a final view
     screen.fill(BLACK)
     all_sprites.draw(screen)
